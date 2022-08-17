0ffc95bed0149ab5b906fc3b1ba7bc1eaeb7cb8d
gonsakon <gonsakon@gmail.com>
2022-08-17 16:47:12 +0800

    update

---
+++ src/components/FAQ.js
@@ -0,0 +1,15 @@
+import {  Link } from "react-router-dom";
+function FAQ(){
+    return (
+      <>
+        <h2>FAQ</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi, numquam?</p>
+        <Link to="/">Home</Link>
+      </>
+    )
+  }
+
+
+export  {
+    FAQ
+};
